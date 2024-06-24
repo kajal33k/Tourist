4691e0a274dffb3d742dc0d09bbeadb1646eaeb5
kajal kumari <2005050100024kajal@gmail.com>
2024-06-24 11:34:29 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+      },
+      animation: {
+        fadeIn: 'fadeIn 2s forwards',
+      },
+    },
+  },
+  plugins: [],
+}
+
